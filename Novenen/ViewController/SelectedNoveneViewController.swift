f3dcdb877325e2de9c5324cb15d8b4730f9bfa3b
Robert Komarek <robert.komarek98@gmail.com>
2023-04-11 23:35:24 +0200

    update 11 April

--- Novenen/ViewController/SelectedNoveneViewController.swift
+++ Novenen/ViewController/SelectedNoveneViewController.swift
@@ -1,46 +1,41 @@
-//
-//  SelectedNoveneViewController.swift
-//  Novenen
-//
-//  Created by Marina Komarek on 31.03.23.
-//
-
 import UIKit
 
-class SelectedNoveneViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class SelectedNoveneViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UIScrollViewDelegate, UITextViewDelegate {
     
     var passedNovene: [Novene] = []
+    var selectedRow : Int?
 
     @IBOutlet weak var textViewNovene: UITextView!
     @IBOutlet weak var pickerViewTage: UIPickerView!
     @IBOutlet weak var labelTagesueberschrift: UILabel!
+    @IBOutlet weak var buttonGoToLitanei: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.pickerViewTage.dataSource = self
         self.pickerViewTage.delegate = self
-        //TEST
+        self.textViewNovene.delegate = self
         
         //Hintergrund-Transparenz der top Navigation Bar entfernen
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
         
         self.navigationItem.title = passedNovene[0].Novenenname
-        textViewNovene.text = passedNovene[0].Tagestext
         
-        //Autoheight for label
         labelTagesueberschrift.numberOfLines = 0
-        [labelTagesueberschrift .sizeToFit]
+        labelTagesueberschrift.sizeToFit()
+        //[labelTagesueberschrift .sizeToFit]
         labelTagesueberschrift.text = passedNovene[0].Tagesueberschrift
         
+        textViewNovene.text = passedNovene[0].Tagestext
         //textViewNovene.layer.cornerRadius = 20.0
         //textViewNovene.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        
+        buttonGoToLitanei.isHidden = true
+        buttonGoToLitanei.layer.cornerRadius = 10.0
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        
-    }
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -51,15 +46,28 @@ class SelectedNoveneViewController: UIViewController, UIPickerViewDataSource, UI
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        selectedRow = row
         return passedNovene[row].Tag
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         //Autoadjust height of label
-        labelTagesueberschrift.numberOfLines = 0
         labelTagesueberschrift.text = passedNovene[row].Tagesueberschrift
+        labelTagesueberschrift.numberOfLines = 0
         [labelTagesueberschrift .sizeToFit]
         textViewNovene.text = passedNovene[row].Tagestext
-         }
+        
+        buttonGoToLitanei.isHidden = true
+     }
+        
+  
+    //check if textview scrolled to end/bottom
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if (scrollView.contentOffset.y >= scrollView.contentSize.height - (scrollView.frame.size.height + 10) && selectedRow != 0) {
+            buttonGoToLitanei.isHidden = false
+        } else {
+            buttonGoToLitanei.isHidden = true
+        }
+    }
 
 }
