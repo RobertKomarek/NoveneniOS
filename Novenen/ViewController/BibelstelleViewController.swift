929a507b62eee1c21ee5ca7cf563aa01dff01b1e
Robert Komarek <robert.komarek98@gmail.com>
2023-05-10 19:51:00 +0200

    Update 10. Mai 19:50

--- Novenen/ViewController/BibelstelleViewController.swift
+++ Novenen/ViewController/BibelstelleViewController.swift
@@ -3,22 +3,43 @@ import DeviceKit
 
 class BibelstelleViewController: UIViewController {
 
+    @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var imageBible: UIImageView!
     @IBOutlet weak var buttonBibelstelleZiehen: UIButton!
     @IBOutlet weak var textViewBibelzitat: UITextView!
     
+    var biblePanorama: [UIImage] = [UIImage(named: "biblePanorama1.jpg")!, UIImage(named: "biblePanorama2.jpg")!, UIImage(named: "biblePanorama3.jpg")!, UIImage(named: "biblePanorama4.jpg")!, UIImage(named: "biblePanorama5.jpg")!, UIImage(named: "biblePanorama6.jpg")!, UIImage(named: "biblePanorama7.jpg")!, UIImage(named: "biblePanorama8.jpg")!, UIImage(named: "biblePanorama9.jpg")!, UIImage(named: "biblePanorama10.jpg")!, UIImage(named: "biblePanorama11.jpg")!, UIImage(named: "biblePanorama12.jpg")!, UIImage(named: "biblePanorama13.jpg")!, UIImage(named: "biblePanorama14.jpg")!, UIImage(named: "biblePanorama15.jpg")!, UIImage(named: "biblePanorama16.jpg")!, UIImage(named: "biblePanorama17.jpg")!, UIImage(named: "biblePanorama18.jpg")!]
+    
+    override func viewDidLayoutSubviews() {
+        textViewBibelzitat.centerVertically()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        imageBible.image = biblePanorama[Int.random(in: 0..<biblePanorama.count)]
+    }
+    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         
-        /*var bibelStelle = BibelStelle()
-        var randomBibelstelle = bibelStelle.getBibelstelle(jsonName: "Einheitsuebersetzung1980")*/
+        /*let screen = UIScreen.main.bounds
+        var gradientView = UIView(frame: CGRect(x: 0, y: 0, width: screen.size.width, height: screen.size.height))
+        
+          let gradientLayer:CAGradientLayer = CAGradientLayer()
+          gradientLayer.frame.size = gradientView.frame.size
+          gradientLayer.colors =
+        [UIColor.lightGray.cgColor, UIColor.darkGray.cgColor, UIColor.black.cgColor, UIColor.black.cgColor, UIColor.black.cgColor, UIColor.black.cgColor, UIColor.black.cgColor, UIColor.black.cgColor]
+        //[UIColor.white.cgColor, UIColor.black.cgColor, UIColor.black.withAlphaComponent(1).cgColor]
+          backgroundView.layer.addSublayer(gradientLayer)*/
         
         buttonBibelstelleZiehen.layer.cornerRadius = 10.0
         buttonBibelstelleZiehen.clipsToBounds = true
         
         textViewBibelzitat.text = "\"Bittet und es wird euch gegeben; sucht und ihr werdet finden; klopft an und es wird euch geöffnet (Matthäus 7,7 und Lukas 11,9)\"\n\n" +
-        "Eine Möglichkeit von Gott eine Antwort in einer bestimmten Frage zu erhalten, ist z.B. das Aufschlagen der Bibel. Zuvor solltest Du ein Gebet verrichten:\n\n" +
         "\"HIMMLISCHER VATER, DEIN WORT IST MEINEM FUß EINE LEUCHTE, EIN LICHT FÜR MEINE PFADE (Psalm 119)\""
+        //"Eine Möglichkeit von Gott eine Antwort in einer bestimmten Frage zu erhalten, ist z.B. das Aufschlagen der Bibel. Zuvor solltest Du ein Gebet verrichten:\n\n" +
      }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -30,13 +51,23 @@ class BibelstelleViewController: UIViewController {
         if segue.identifier == "segueRandomBibelstelle" {
             
             let bibelStelle = BibelStelle()
-            //var randomBibelstelle = bibelStelle.getBibelstelle(jsonName: "Einheitsuebersetzung1980")
            var kompletteBibel = bibelStelle.getBibelstelle(jsonName: "Einheitsuebersetzung1980")
             
             var selectedBibelstelleVC = SelectedBibelstelleViewController()
             selectedBibelstelleVC = segue.destination as! SelectedBibelstelleViewController
-            //selectedBibelstelleVC.randomBibelstelle = randomBibelstelle
             selectedBibelstelleVC.randomBibelstelle = kompletteBibel
         }
     }
 }
+
+extension UITextView {
+    func centerVertically() {
+        let fittingSize = CGSize(width: bounds.width, height: CGFloat.greatestFiniteMagnitude)
+        let size = sizeThatFits(fittingSize)
+        let topOffset = (bounds.size.height - size.height * zoomScale) / 2
+        let positiveTopOffset = max(1, topOffset)
+        contentOffset.y = -positiveTopOffset
+    }
+}
+
+
