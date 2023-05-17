386837661173ebda37925120516b023fc89ddb63
Robert Komarek <robert.komarek98@gmail.com>
2023-05-18 00:30:32 +0200

    Update 18.5. 00:30

--- Novenen/ViewController/ImpressumViewController.swift
+++ Novenen/ViewController/ImpressumViewController.swift
@@ -9,21 +9,43 @@ import UIKit
 
 class ImpressumViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var textViewBeispieltext: UITextView!
+    
+    @IBAction func stepperSchriftgroesseClicked(_ sender: Any) {
+        
+        let stepper = sender as! UIStepper
+        print(stepper.value)
+        print(stepper.minimumValue)
+        print(stepper.maximumValue)
+        
+        if stepper.value < stepper.minimumValue {
+            //get current Font Size of Example Text
+            if let font = textViewBeispieltext.font {
+                var fontSize = font.pointSize
+                fontSize = fontSize - 1.0
+                // Create a new UIFont object with the desired font size
+                let newFont = textViewBeispieltext.font?.withSize(fontSize)
+                textViewBeispieltext.font = newFont
+            }
+        }
+        
+        if stepper.value > stepper.maximumValue {
+            //get current Font Size of Example Text
+            if let font = textViewBeispieltext.font {
+                var fontSize = font.pointSize
+                //print("current fontsize: \(fontSize)")
+                fontSize = fontSize + 1.0
+                //print("new fontsize: \(fontSize)")
+                // Create a new UIFont object with the desired font size
+                let newFont = textViewBeispieltext.font?.withSize(fontSize)
+                textViewBeispieltext.font = newFont
+            }
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
     }
-    */
-
 }
